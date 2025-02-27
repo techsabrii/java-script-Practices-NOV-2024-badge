cb2ba0c62867845c8516f50b6756ca8985f21971
TechSabrii <bk83835@gmail.com>
2025-02-27 10:59:21 +0500

    First cmmit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+// var element;
+// element = document.URL;
+// console.log(element);
+
+
+// var element;
+// document.getElementById("header").setAttribute("style","color: blue");
+// var element = document.getElementById("header").innerHTML; 
+// console.log(element);
+
+// document.getElementById("header").innerHTML = "Hello World";
+
+// document.getElementById("header").setAttribute("style","brown");
+
+// element = document.getElementById("header").setAttribute('style');
+
+// console.log(element);
+
+
+// var element;
+// document.querySelector("h1").setAttribute("style","color: green");
+// var element = document.getElementById("header").innerHTML; 
+// console.log(element);
+ 
+
+
+// css style method
+
+// var element;
+//  document.querySelector("main").style.marginTop = "100px";
+// var element = document.querySelector("#header").style.color;
+// console.log(element);
+
+// style
+// var element;
+//  document.querySelector("#header").style.color = "green";
+// var element = document.querySelector("#header").style.color;
+// console.log(element);
+
+
+// classname
+
+// var element;
+//  document.querySelector("#header").className = "xyz";
+// var element = document.querySelector("#header").style.color;
+// console.log(element);
+
+
+// classList list of array
+
+// var element;
+//  document.querySelector("#header").classList.add("xyz","vbs");
+// var element = document.querySelector("#header").style.color;
+// console.log(element);
+
+// function method
+
+
+// var element;
+//  document.querySelector("#header").onclick = xyz;
+
+
+//  function xyz(){
+    
+//     var element = document.querySelector("#header").classList.add("xyz","vbs");
+// }
+
+
+// addEventListener method
+
+
+var element;
+ document.querySelector("#header").addEventListener("click", xyz);
+
+
+ function xyz(){
+    
+    this.classList.add("xyz","vbs");
+}
+
+
+
